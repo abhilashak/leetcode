8411c1f38d3f6fac70dc75e0d5451a0022a02fab
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-13 20:48:11 +0530

    test: add second test case for prices with length one for problem #2

--- buy_sell_stock/test_buy_sell.rb
+++ buy_sell_stock/test_buy_sell.rb
@@ -14,4 +14,8 @@ class TestBuySellStock < Minitest::Test
   def test_array_is_an_empty_array
     assert_equal 'Provide an array of two or more elements', BuySellStock.new([]).max_profit
   end
+
+  def test_array_with_length_one
+    assert_equal 'Provide an array of two or more elements', BuySellStock.new([1]).max_profit
+  end
 end
