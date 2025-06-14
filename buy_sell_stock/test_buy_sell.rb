b0c7d7d149a135d79715f8edf98ed2439f110e3d
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-14 07:35:51 +0530

    test: add test case for prices length 5 and 7 for buy sell stock problem #2

--- buy_sell_stock/test_buy_sell.rb
+++ buy_sell_stock/test_buy_sell.rb
@@ -10,6 +10,10 @@ class TestBuySellStock < Minitest::Test
     ####
   end
 
+  def create_array(length)
+    Array.new(length) { rand(1..100) }
+  end
+
   # ex: BuySellStock.new([]).max_profit
   def test_array_is_an_empty_array
     assert_equal 'Provide an array of two or more elements', BuySellStock.new([]).max_profit
@@ -27,5 +31,15 @@ class TestBuySellStock < Minitest::Test
   def test_array_with_length_three
     assert_equal 5, BuySellStock.new([1, 3, 6]).max_profit
     assert_equal 0, BuySellStock.new([5, 3, 2]).max_profit
+  end
+
+  def test_array_with_length_five
+    assert_equal 22, BuySellStock.new([1, 3, 6, 23, 2]).max_profit
+    assert_equal 12, BuySellStock.new([15, 25, 8, 20]).max_profit
+  end
+
+  def test_array_with_length_seven
+    assert_equal 11, BuySellStock.new([10, 13, 16, 9, 18, 3, 14]).max_profit
+    assert_equal 12, BuySellStock.new([9, 13, 6, 16, 9, 18, 12]).max_profit
   end
 end
