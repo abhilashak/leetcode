ce47ef364add1e7ff6eeba3a2b4a2ccec0764d44
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-14 23:49:54 +0530

    refactor: add comments problem #3

--- array_duplicate/test_array_duplicate.rb
+++ array_duplicate/test_array_duplicate.rb
@@ -3,8 +3,16 @@
 require 'minitest/autorun'
 require_relative 'array_duplicate'
 ######################################
+# Given an integer array nums, return true if any value appears at least twice in the array,
+# and return false if every element is distinct.
 #
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
 #
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
 #
 ######################################
 class TestArrayDuplicate < Minitest::Test
