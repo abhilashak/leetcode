54d570f40a7e4a0235b7604ff34a94e57befc59d
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-15 12:44:18 +0530

    test: add first failing test case for product of array except self problem #4"

---
+++ product_except_self/test_product_except_self.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'product_except_self'
+################
+# Product of Array Except Self
+#
+# Given an integer array nums, return an array answer such that answer[i] is equal to
+# the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+################
+class TestProductExceptSelf < Minitest::Test
+  def set_up
+    ###
+  end
+
+  def test_empty_array
+    assert_equal 'Provide an aaray of length atleast two', ProductNumbers.new([]).except_self
+  end
+end
