66a9049fdcd537cfb827c12abb1847c7584a76a7
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-15 21:20:11 +0530

    test: add test cases for optimized solution problem #4

--- product_except_self/test_product_except_self.rb
+++ product_except_self/test_product_except_self.rb
@@ -17,24 +17,63 @@ class TestProductExceptSelf < Minitest::Test
 
   def test_empty_array
     assert_equal 'Provide an array of length atleast two', Numbers.new([]).product_except_self
+    assert_equal 'Provide an array of length atleast two', Numbers.new([]).product_except_self_optimized
   end
 
   def test_array_of_length_one
     assert_equal 'Provide an array of length atleast two', Numbers.new([4]).product_except_self
+    assert_equal 'Provide an array of length atleast two', Numbers.new([4]).product_except_self_optimized
   end
 
   def test_array_of_length_two
     assert_equal [3, 4], Numbers.new([4, 3]).product_except_self
     assert_equal [6, 5], Numbers.new([5, 6]).product_except_self
+
+    # Test optimized version
+    assert_equal [3, 4], Numbers.new([4, 3]).product_except_self_optimized
+    assert_equal [6, 5], Numbers.new([5, 6]).product_except_self_optimized
   end
 
   def test_array_of_length_three
     assert_equal [6, 3, 2], Numbers.new([1, 2, 3]).product_except_self
     assert_equal [15, 20, 12], Numbers.new([4, 3, 5]).product_except_self
+
+    # Test optimized version
+    assert_equal [6, 3, 2], Numbers.new([1, 2, 3]).product_except_self_optimized
+    assert_equal [15, 20, 12], Numbers.new([4, 3, 5]).product_except_self_optimized
   end
 
   def test_array_of_length_four
     assert_equal [70, 140, 56, 40], Numbers.new([4, 2, 5, 7]).product_except_self
     assert_equal [216, 54, 36, 24], Numbers.new([1, 4, 6, 9]).product_except_self
+
+    # Test optimized version
+    assert_equal [70, 140, 56, 40], Numbers.new([4, 2, 5, 7]).product_except_self_optimized
+    assert_equal [216, 54, 36, 24], Numbers.new([1, 4, 6, 9]).product_except_self_optimized
+  end
+
+  def test_leetcode_examples
+    # Example 1: [1,2,3,4] -> [24,12,8,6]
+    assert_equal [24, 12, 8, 6], Numbers.new([1, 2, 3, 4]).product_except_self_optimized
+
+    # Example 2: [-1,1,0,-3,3] -> [0,0,9,0,0]
+    assert_equal [0, 0, 9, 0, 0], Numbers.new([-1, 1, 0, -3, 3]).product_except_self_optimized
+  end
+
+  def test_both_methods_give_same_results
+    test_cases = [
+      [4, 3],
+      [1, 2, 3],
+      [4, 2, 5, 7],
+      [1, 4, 6, 9],
+      [-1, 1, 0, -3, 3],
+      [2, 3, 4, 5]
+    ]
+
+    test_cases.each do |nums|
+      original_result = Numbers.new(nums).product_except_self
+      optimized_result = Numbers.new(nums).product_except_self_optimized
+      assert_equal original_result, optimized_result, "Results don't match for #{nums}"
+    end
   end
 end
