43d2da297a2d3f487f5e8608bb4fa5285d878f7b
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-15 13:29:47 +0530

    fix(problem): array with length two and three for product of array except self problem #4

--- product_except_self/product_except_self.rb
+++ product_except_self/product_except_self.rb
@@ -7,11 +7,38 @@
 # the product of all the elements of nums except nums[i].
 # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 # You must write an algorithm that runs in O(n) time and without using the division operation.
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+
+# Constraints:
+
+# 2 <= nums.length <= 105
+# -30 <= nums[i] <= 30
+# The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+
+# Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+#
+# Ex: Numbers.new([2,3,4]).product_except_self
 ################
-class ProductNumbers
+class Numbers
   def initialize(nums)
     @numbers = nums
   end
 
-  def except_self; end
+  def product_except_self
+    return 'Provide an aaray of length atleast two' if @numbers.length < 2
+
+    answer = []
+    @numbers.each do |number|
+      answer << @numbers.reject { |ele| ele == number }.sum
+    end
+
+    answer
+  end
 end
