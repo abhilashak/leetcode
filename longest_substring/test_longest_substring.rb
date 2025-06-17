006c68405ea9b619d88e7d1550a47cd834a5be44
Abhilash A K <mailto.abhilashak@gmail.com>
2025-06-17 21:28:52 +0530

    test(problem): add first failing test case for longest substring problem #6

---
+++ longest_substring/test_longest_substring.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'longest_substring'
+#################################
+## Example 1:
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+#################################
+class TestLongestSubstring < Minitest::Test
+  def setup
+    ####
+  end
+
+  def test_empty_array
+    assert_equal 0, Substring.new('').longest
+  end
+end
